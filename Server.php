81d41b43f2804f739e64fcaff6823811bcf58b27
aquarink <junkuwieazy@gmail.com>
2020-01-06 12:41:32 +0700

    Play Start and End logic

--- Server.php
+++ Server.php
@@ -5,11 +5,9 @@ use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
 use Ratchet\ConnectionInterface;
 use Ratchet\MessageComponentInterface;
-// use MyApp\Socket;
 use Predis\Client;
 
 require dirname( __FILE__ ) . '/vendor/autoload.php';
-// require dirname( __FILE__ ) . '/Socket.php';
 
 class Socket implements MessageComponentInterface {
 
@@ -68,6 +66,7 @@ class Socket implements MessageComponentInterface {
                 foreach ($getRoomKeyData as $key => $resourceId) {
                     if(isset($this->clients[$resourceId])) {
                         $this->clients[$resourceId]->send("GM : [".$dataQs['playerName']."] connected \n");
+                        $this->clients[$resourceId]->send("[BUTTON_PLAY] : TRUE \n");
                     }
                 }
             } else {
@@ -94,10 +93,56 @@ class Socket implements MessageComponentInterface {
 
         // print_r($getRoomKeyData);
 
+        // INIT
+        $chat = '';
+        $play = false;
+
+        // PARSE PESAN
+        $expl_mgg = explode('***', $msg);
+        if($expl_mgg[0] == 'chat') {
+            $chat = "[".$expl_player[1]."] : ".$expl_mgg[1]." \n";
+        } elseif($expl_mgg[0] == 'play') {
+
+            if($expl_mgg[1] == 'start') {
+                // GET DATA FROM PLAY
+                $playData = $redis->get('PLAY:'.$playerData);
+                if($playData == '') {
+                    // // INIT PLAY THE GAME BIND ROOM
+                    $redis->set('PLAY:'.$playerData, $from->resourceId);
+                    $play = "[BUTTON_PLAY] : FALSE ".$expl_player[1]." is Playing \n";
+                } else {
+                    // GET DATA PLAY ROOM DATA
+                    $getPlayRoomData = $redis->hget('ROOMS:'.$playerData, $playData);
+                    // EXPLODE PLAY DATA
+                    $expl_play = explode(':', $getPlayRoomData);
+                    $this->clients[$from->resourceId]->send("[BUTTON_PLAY] : FALSE ".$expl_play[1]." is Playing \n");
+                }
+            } elseif($expl_mgg[1] == 'end') {
+                // GET DATA FROM PLAY
+                $playData = $redis->get('PLAY:'.$playerData);
+                if($playData == $from->resourceId) {
+                    $redis->set('PLAY:'.$playerData, '');
+                    $play = "[BUTTON_PLAY] : TRUE please push \n";
+                } else {
+                    // GET DATA PLAY ROOM DATA
+                    $getPlayRoomData = $redis->hget('ROOMS:'.$playerData, $playData);
+                    // EXPLODE PLAY DATA
+                    $expl_play = explode(':', $getPlayRoomData);
+                    $play = "[BUTTON_PLAY] : FALSE ".$expl_play[1]." is Playing please wait \n";
+                }
+            } else {
+                $play = "Perintah bermain salah \n";
+            }           
+        }
+
         // BROADCAST
         foreach ($getRoomKeyData as $key => $resourceId) {
             if(isset($this->clients[$resourceId])) {
-                $this->clients[$resourceId]->send("[".$expl_player[1]."] : ".$msg." \n");
+                if($chat != '') {
+                    $this->clients[$resourceId]->send($chat);
+                } elseif($play != '') {
+                    $this->clients[$resourceId]->send($play);
+                }
             }
         }
     }
